234a2f819ef6eced5d53f3283b35696950b4eb22
Keisuke Nagakawa <keisuke.n.37@gmail.com>
2019-02-17 23:31:07 +0900

    ip設定

--- curiosity2/settings.py
+++ curiosity2/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '_^cf&c9s$$@ahtr_-e+_lpckecq4kyk7*0&bdz^si!%n3tp!sz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '[::1]', '192.168.11.2']
 
 # Application definition
 
