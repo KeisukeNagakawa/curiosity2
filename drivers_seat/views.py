e67f3eeea8a26b6994318242c5bb470ea483ad56
Keisuke Nagakawa <keisuke.n.37@gmail.com>
2019-03-10 23:34:44 +0900

    changed a bit

--- drivers_seat/views.py
+++ drivers_seat/views.py
@@ -12,29 +12,29 @@ def drivers_view(request):
 
 def forward(request):
     print("forward function is called in view.py")
-    GPIO.cdc(20, 'L')
-    GPIO.cdc(20, 'R')
+    GPIO.cdc(-40, 'L')
+    GPIO.cdc(-40, 'R')
     return HttpResponse('Return data to ajax call')
 
 
 def backward(request):
     print("backward function is called in view.py")
-    GPIO.cdc(-20, 'L')
-    GPIO.cdc(-20, 'R')
+    GPIO.cdc(20, 'L')
+    GPIO.cdc(20, 'R')
     return HttpResponse('Return data to ajax call')
 
 
 def left(request):
     print("left function is called in view.py")
-    GPIO.cdc(-20, 'L')
-    GPIO.cdc(20, 'R')
+    GPIO.cdc(+20, 'L')
+    GPIO.cdc(-40, 'R')
     return HttpResponse('Return data to ajax call')
 
 
 def right(request):
     print("right function is called in view.py")
-    GPIO.cdc(20, 'L')
-    GPIO.cdc(-20, 'R')
+    GPIO.cdc(-40, 'L')
+    GPIO.cdc(20, 'R')
     return HttpResponse('Return data to ajax call')
 
 
